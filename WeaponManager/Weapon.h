bcaf2fc2cab28a38fd723e48a79eb886d7a61d59
TooT <theo.p-h@live.fr>
2023-03-03 19:04:43 +0100

    Wepon Manager

--- WeaponManager/Weapon.h
+++ WeaponManager/Weapon.h
@@ -5,8 +5,53 @@
 class Weapon
 {
     public:
+        enum Type
+            {
+                Laser1,
+                Laser2,
+                Laser3,
+                Missile,
+                WeaponsCount
+            };
+
+            struct Canon
+            {
+                Canon(sf::Vector2f position, float angle, Projectile::Type projectile, Animation::Type blast) :
+                    position(position),
+                    angle(angle),
+                    projectile(projectile),
+                    blast_Anim(blast)
+                {
+                }
+
+                sf::Vector2f position;
+                float angle;
+                Projectile::Type projectile;
+                Animation::Type blast_Anim;
+            };
+
+    public:
         Weapon();
 
+    private:
+
+        Weapon::Type mType;
+
+        float  mFireRate;        //Cadence de tir en tir/s
+        std::vector<Cannon> mCannons; // x, y , theta
+
+        bool mNeedAmmo;
+        int mAmmo;
+
+        Command mFireCommand;
+
+        bool mFire;
+        Timer mReloadTimer;
+        sf::Sprite mSprite;
+        Sounds::ID mBlastSound;
+
+        //Textures et anim
+        std::vector<Animation> mBlastAnims;
 
 };
 
